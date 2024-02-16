f276ce1055cccb6c5f377ea2ac0bfd7aab41124b
sarahaptumich <112976778+sarahaptumich@users.noreply.github.com>
2024-02-15 21:33:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,22 @@ with st.form(key="my_form",clear_on_submit=True):
     
     st.write("Enter Brewing Metrics")
     
-    stock_ticker_input = st.text_input('Stock', key='ticker')
-    note_input = st.text_input('Note', key='note')
+    bean_name_input = st.text_input('Bean Name', key='ticker')
+    grams_input = st.text_input('Grams', key='note')
+    grind_input = st.text_input('Grind', key='note2')
+    start_pour_input = st.text_input('Start Pour', key='note3')
+    end_pour_input = st.text_input('End Pour', key='note4')
+    date_input = st.text_input('DAate', key='note5')
     
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("Note", note_input, "stock_ticker", stock_ticker_input)
-        form_callback(stock_ticker_input,note_input)
+        st.write("Bean Name", bean_name_input,
+         "Grams", grams_input,
+         "Grind", grind_input,
+         "Start Pour", start_pour_input,
+         "End Pour", end_pour_input,
+         "Date", date_input)
+        form_callback(bean_name_input, grams_input, grind_input, start_pour_input, end_pour_input, date_input)
 
 st.info(" #### Show contents of the CSV file :point_down:")
-st.dataframe(pd.read_csv("notes.csv",names=["Stock","Note"]),height=300)
+st.dataframe(pd.read_csv("espresso.csv",names=["Bean_Name","Grams", "Grind", "Start_Pour", "End_Pour", "Date"]),height=300)
